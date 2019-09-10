36dd53352bd57b7687d7776b5138fde7526d5feb
Maksym <maksym.kotsupyr@gmail.com>
2019-09-11 02:23:37 +0300

    Lesson13

---
+++ Lesson13/js/script.js
@@ -0,0 +1,21 @@
+var name = 'Maksym', surname = ' Kotsupyr', age = ' (19y.o.)';
+student = name + surname;
+student +=age;
+
+console.log(student);
+
+var a = 11, b = 2;
+c = a % b;
+console.log(c);
+
+var a = 36, b = 3;
+c = a % b;
+console.log(c);
+
+var a = "5", b = 2;
+c = a + b;
+console.log(c);
+
+var a = "hello", b = 5;
+c = a + b;
+console.log(c);
